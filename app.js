025b54a38d547f09d89f4ec71e69f35edc9e6f0e
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-09 00:18:12 +0545

    created server first request from server and first response from server

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
  const http=require('http')
- function rqListner(req,res){}
+
  const server=http.createServer((req,res)=>{
-console.log(req)
+    // requesting data
+    console.log(req.url,req.method,req.headers);
+
+    //sending response to the webpage
+    res.setHeader('Content-Type','text/html')//browser knows html file is being sent back in resoponse.
+    res.write('<html>')
+    res.write('<head><title>My first Node.js Page</title></head>')
+    res.write('<body><h2>i am writing the node js project</h2></body>')
+    res.write('</html>')
+    res.end()
  });
 server.listen(5000,()=>{
-    console.log("Server is running at port 3000")
+    console.log("Server is running at port 5000")
 });
