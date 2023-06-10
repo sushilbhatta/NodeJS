823e40aa00e05eb7b670f00ac848c4703b07f8e6
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-10 23:58:11 +0545

    body-parser

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ app.use('/',(req,res,next)=>{
 })
 
 app.use('/add-product',(req,res,next)=>{
-    res.send('<h1>Welcome to add-product page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+app.use('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
 app.use('/',(req,res,next)=>{
     console.log('Second middleware')
