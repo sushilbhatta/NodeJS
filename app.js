39ecf490307fad8b9c3087e05d2065c87cf7be49
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-10 22:07:06 +0545

    using express

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express=require('express');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log("First Middleware")
-    next(); //this allows to continue the req to the next middleware.
-});
-app.use((req,res,next)=>{
-    console.log('Second middleware')
-    res.send('<h1>Hello from express</h1>')
+app.use('/',(req,res,next)=>{
+    console.log('this always runs')
+    next()
 })
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<h1>Welcome to add-product page</h1>')
+})
+app.use('/',(req,res,next)=>{
+    console.log('Second middleware')
+    res.send('<h1>Helo from express</h1>')
+})
+
 
 app.listen(5000,()=>{
     console.log('port is running at port  5000')
