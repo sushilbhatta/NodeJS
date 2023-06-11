01fd730a47ac70a6df5ef4d7ad9300a65a489657
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-11 12:05:19 +0545

    static

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express=require('express');
 
 const adminRoutes=require('./routes/admin')
 const shopRoutes=require('./routes/shop')
-const app=express();
+
+const app=express()
+
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
  
 app.use('/admin',adminRoutes)
 app.use(shopRoutes)
