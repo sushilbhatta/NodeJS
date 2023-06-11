9b129fd52427f0e3cf2234585234fd5b58f50de5
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-11 09:33:55 +0545

    filtering routes

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const shopRoutes=require('./routes/shop')
 const app=express();
 app.use(bodyParser.urlencoded({extended:false}))
  
-app.use(adminRoutes)
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).send('Page not found')
+})
 
 app.listen(5000,()=>{
     console.log('port is running at port  5000')
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const router=express.Router()
 
-router.use('/',(req,res,next)=>{
+router.get('/',(req,res,next)=>{
     // console.log('Second middleware')
     res.send('<h1>Helo from express</h1>')
 })
