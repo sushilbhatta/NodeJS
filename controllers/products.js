3e253c6571201d37bca576742d8aa3583265e511
sushilbhatta <sushilbhatta58@gmail.com>
2023-06-15 12:19:47 +0545

    model

--- controllers/products.js
+++ controllers/products.js
@@ -1,32 +1,30 @@
-const Product=require('../models/product')
-// const products=[]
-exports.getAddProduct=(req,res,next)=>{
-    res.render('add-product',{
-        docTitle:'add-product',
-        path:'/admin/add-product',
-        formsCSS:true,
-        productsCSS:true,
-        activeAddProduct:true
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    docTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      docTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
-    // res.sendFile(path.join(rootDir,'views','add-product.html'))
-}
-exports.postAddProduct=(req,res)=>{
-    const product = new Product(req.body.title);
-    product.save();
-    // products.push({title:req.body.title})
-    res.redirect('/')
-} 
-exports.getProducts=(req,res,next)=>{
-    const products=Product.fetchAll();
-    res.render('shop',{
-        prods:products,
-        docTitle:'Shop',
-        path:'/',
-        hasProducts:products.length>0,
-        activeShop:true,
-        productCSS:true,
-    })
-    // console.log(prods)
-    // console.log(adminData.product)
-    // res.sendFile(path.join(rootDir,'views','shop.html'))
-}
+  });
+};
--- models/product.js
+++ models/product.js
@@ -1,13 +1,37 @@
-const products=[]
+const fs = require('fs');
+const path = require('path');
 
-module.exports=class Product{
-    constructor(t){
-        this.title=t
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    save(){
-        products.push(this);
-    }
-    static fetchAll(){
-        return products
-    }
-}
+  });
+};
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
