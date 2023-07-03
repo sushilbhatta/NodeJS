3f9e24baf872d89a6539810e3d8a9d20cc20b535
sushilbhatta <sushilbhatta58@gmail.com>
2023-07-03 22:36:12 +0545

    working with data

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,13 @@ exports.getAddProduct = (req, res, next) => {
     });
   };
   exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title=req.body.title
+    const imgUrl=req.body.imgUrl;
+    const price=req.body.price;
+    const description=req.body.description
+
+    console.log(title,imgUrl,price,description)
+    const product = new Product(title,imgUrl,price,description);
     product.save();
     res.redirect('/');
   };
